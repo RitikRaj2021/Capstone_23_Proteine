6c5629b0eddcdcd5bf55467a4dacad6822279271
BarryHennett <harrybennettnz@gmail.com>
2023-05-04 16:10:45 +1200

    landing page and fix logout size

--- Capstone_23_Proteine/Controllers/HomeController.cs
+++ Capstone_23_Proteine/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace Capstone_23_Proteine.Controllers
             _logger = logger;
         }
 
+        public IActionResult Landing()
+        {
+            return View();
+        }
+
         public IActionResult Index()
         {
             return View();
