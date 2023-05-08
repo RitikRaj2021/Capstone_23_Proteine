abb47c1200c665ce7f80e2fbbb749559930cd8e8
ChristopherDronjak <yoschmoo@gmail.com>
2023-05-09 08:51:09 +1200

    About Me Backend
    Backend implemented on about me page

--- Capstone_23_Proteine/Data/ApplicationDbContext.cs
+++ Capstone_23_Proteine/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Capstone_23_Proteine.Models.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Capstone_23_Proteine.Data
@@ -9,5 +10,7 @@ namespace Capstone_23_Proteine.Data
             : base(options)
         {
         }
+
+        public DbSet<AboutMe> AboutMe { get; set; }
     }
 }
