423ffbec5de798201b63ebb089d7ef1ff05feda7
ChristopherDronjak <yoschmoo@gmail.com>
2023-05-16 11:24:39 +1200

    Email API sends email when registering but missing confirm
    When registering to the website an email is sent to your email but there is still currently no confirmation/verification in the email (needs to be implemented)

---
+++ Capstone_23_Proteine/Services/EmailSender.cs
@@ -0,0 +1,23 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Capstone_23_Proteine.Services
+{
+    public class EmailSender
+    {
+        public async Task SendEmail(string subject, string email, string message)
+        {
+            var apiKey = "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";
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("proteine.team@gmail.com", "Proteine Team");
+            var to = new EmailAddress(email);
+            var plaintextContent = message;
+            var htmlContent = "";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plaintextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+
+        }
+    }
+}
