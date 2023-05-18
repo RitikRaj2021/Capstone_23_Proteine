a79e961caf67d8de3abce1801d33e36009d0e08e
RitikRaj2021 <ritikr379@gmail.com>
2023-05-19 10:55:10 +1200

    Reran migration jobs to add tables back after deletion

--- Data/Migrations/20230518224622_addingTabalesBack1.Designer.cs
+++ Data/Migrations/20230518224622_addingTabalesBack1.Designer.cs
@@ -4,6 +4,7 @@ using Capstone_23_Proteine.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Capstone_23_Proteine.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230518224622_addingTabalesBack1")]
+    partial class addingTabalesBack1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
