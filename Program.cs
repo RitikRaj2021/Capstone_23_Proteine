aa677acc30aaf042e82fa281e23f8b4e5c707dfc
RitikRaj2021 <ritikr379@gmail.com>
2023-05-19 17:16:37 +1200

    Changed confirmation required to false in program cs

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ public class Program
             options.UseSqlServer(connectionString));
         builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-        builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+        builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
             .AddRoles<IdentityRole>()
             .AddEntityFrameworkStores<ApplicationDbContext>();
         builder.Services.AddControllersWithViews();
