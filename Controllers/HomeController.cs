82c22c632dd611ba55c02975173e075045eed193
RitikRaj2021 <ritikr379@gmail.com>
2023-05-22 15:52:05 +1200

    Added function to show total calories for today in daily summary

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,17 +4,21 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Razor.Compilation;
 using System.Data;
 using System.Diagnostics;
+using Capstone_23_Proteine.Data; // Import the namespace that contains ApplicationDbContext
+using Capstone_23_Proteine.Models.Domain; // Import the namespace that contains FoodIntake
 
 namespace Capstone_23_Proteine.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context; // Add the _context field
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
-        }
+            _context = context; // Initialize the _context field
+        } 
 
         // GET: /Home/Landing
         public IActionResult Landing()
@@ -25,8 +29,24 @@ namespace Capstone_23_Proteine.Controllers
         // GET: /Home/Index
         public IActionResult Index()
         {
+            int totalCalories = CalculateTotalCalories(); // Calculate the total calories
+            ViewBag.TotalCalories = totalCalories; // Set the totalCalories value in the ViewBag
+
             return View();
         }
+
+        // CalculateTotalCalories Function
+        private int CalculateTotalCalories()
+        {
+            // Logic  to calculate the total calories
+            DateTime today = DateTime.Today;
+            int totalCalories = _context.FoodIntake
+                .Where(f => f.Date.Date == today)
+                .Sum(f => f.Calories);
+            // Return the calculated total calories
+            return totalCalories;
+        }
+
 
         // GET: /Home/Privacy
         [Authorize(Roles = "Admin")]
