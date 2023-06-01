ce121d567a6b59c892c4856e5e3eaed1e02cd222
ChristopherDronjak <yoschmoo@gmail.com>
2023-06-02 11:09:53 +1200

    Added new column to foodintake table
    added new column called meal type in food intake, changed code in several foodintake related files and created a new migration

--- Models/Domain/FoodIntake.cs
+++ Models/Domain/FoodIntake.cs
@@ -8,6 +8,7 @@ namespace Capstone_23_Proteine.Models.Domain
         public int Protein { get; set; }
         public int Calories { get; set; }
         public int Fat { get; set; }
+        public string MealType { get; set; }
         public string MealName { get; set; }
         public DateTime Date { get; set; }
         public string UserId { get; set; } // Foreign key to IdentityUser
