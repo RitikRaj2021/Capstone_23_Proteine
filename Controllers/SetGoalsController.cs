fb0994aada09c706bd116c0c65f05a9fd3d7d3c6
RitikRaj2021 <ritikr379@gmail.com>
2023-06-06 14:28:12 +1200

    setup so user is able update single entry in setGoals

--- Controllers/SetGoalsController.cs
+++ Controllers/SetGoalsController.cs
@@ -33,29 +33,44 @@ namespace Capstone_23_Proteine.Controllers
         // POST: /SetGoals/SetGoals
         [HttpPost]
         public async Task<IActionResult> SetGoals([Bind("SetProtein, SetCalories, SetFat")] SetGoalsViewModel setGoalsRequest)
-
         {
             // Retrieve the current user
             var user = await userManager.GetUserAsync(User);
             var userId = user.Id;
 
-            // Create a new SetGoals object with the user-provided data
-            var setGoals = new SetGoals()
-            {
-                Id = Guid.NewGuid(),
-                UserId = userId,
-                SetProtein = setGoalsRequest.SetProtein,
-                SetCalories = setGoalsRequest.SetCalories,
-                SetFat = setGoalsRequest.SetFat,
-               
-            };
+            // Check if a record for the current user already exists in the database
+            var existingRecord = await applicationDbContext.SetGoals.FirstOrDefaultAsync(g => g.UserId == userId);
 
-            // Add the SetGoals object to the SetGoals DbSet and save changes to the database
-            await applicationDbContext.SetGoals.AddAsync(setGoals);
-            await applicationDbContext.SaveChangesAsync();
+            if (existingRecord != null)
+            {
+                // Update the existing record with the user-provided data
+                existingRecord.SetProtein = setGoalsRequest.SetProtein;
+                existingRecord.SetCalories = setGoalsRequest.SetCalories;
+                existingRecord.SetFat = setGoalsRequest.SetFat;
+
+                // Save changes to the database
+                await applicationDbContext.SaveChangesAsync();
+            }
+            else
+            {
+                // Create a new SetGoals object with the user-provided data
+                var setGoals = new SetGoals()
+                {
+                    Id = Guid.NewGuid(),
+                    UserId = userId,
+                    SetProtein = setGoalsRequest.SetProtein,
+                    SetCalories = setGoalsRequest.SetCalories,
+                    SetFat = setGoalsRequest.SetFat,
+                };
+
+                // Add the SetGoals object to the SetGoals DbSet and save changes to the database
+                await applicationDbContext.SetGoals.AddAsync(setGoals);
+                await applicationDbContext.SaveChangesAsync();
+            }
 
             // Redirect to the Index action to display the updated food intake records
-            return RedirectToAction();
+            return RedirectToAction("Index", "Home");
         }
+
     }
 }
